5073fba7d78a3735d0fb027b99871131def01170
CypherDev <abdurraheempelumi@gmail.com>
2024-11-26 12:16:59 +0100

    sighhhh

--- src/Components/MainDashboard/UserProfile/UserProfile.jsx
+++ src/Components/MainDashboard/UserProfile/UserProfile.jsx
@@ -222,7 +222,7 @@ const UserProfile = () => {
     const reference = "WibA" + Math.floor(Math.random() * 1000000000 + 1);
     const apiKey = import.meta.env.VITE_MONNIFY_API_KEY;
     const contractCode = import.meta.env.VITE_MONNIFY_CONTRACT_CODE;
-    const isTestMode = process.env.NODE_ENV !== "production"; // Use true for non-production
+    // const isTestMode = process.env.NODE_ENV !== "production"; // Use true for non-production
 
     window.MonnifySDK.initialize({
       amount: 1550,
@@ -234,8 +234,7 @@ const UserProfile = () => {
       apiKey,
       contractCode,
       paymentDescription: "Test Monnify",
-      // isTestMode: true,
-      isTestMode,
+      isTestMode: true,
       metadata: {
         name: userPaymentData.customerFullName.split(" ")[0],
         email: userPaymentData.customerEmail,
